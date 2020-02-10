6094ba7288cac8b2f4f22db7e9dea4f3a95d5c27
arielfrancolima <ghazijj98@gmail.com>
2020-02-10 17:13:02 -0300

    Ejercicio 4 esta completo

--- index.js
+++ index.js
@@ -1,39 +1,38 @@
-function lista(lista){
-    let listaDuplicada = [];
-    for (i=0; i<lista.length; i++){
-        listaDuplicada.push (lista[i] * 2); 
-    }
-    return listaDuplicada; 
-    console.log(listaDuplicada);
-}
-
-function lista(lsita1){
-    lista.map 
-}
-
-//Ejercicio 2
-/* 
-function definirFechas (){
-    let fecha = new Date ();
-    let dia = fecha.getDate();
-    let mes = fecha.getMonth();
-    let anio = fecha.getFullYear(); 
-
-    if (dia < 10) { dia= '0'+ dia);
-    if (mes < 10) {mes = '-'}
-    }
-*/
-//Ejercicio 3 
-
-
-function daysToFinishYear (){ 
-date = new Date();
-cantDias = 31; 
-let day = fecha.getDate() + cantDias;//metodo que indica el dia actual
-const year = 366; 
-anio - cantDias;
-
-console.log (year - day);
+function objectEqual(objectOne, objectTwo) {
+  
+    const keysOne = Object.keys(objectOne);
+    const keysTwo = Object.keys(objectTwo);
+    if(keysOne.length === keysTwo.length) {
+      let equal;
+      let valueOne; 
+      let valueTwo;
+      
+      for(let i = 0; i<keysOne.length; i++){
+        valueOne = objectOne[keysOne[i]];
+        valueTwo = objectTwo[keysTwo[i]];
     
-}
-
+        if(keysOne[i] === keysTwo[i] && valueOne == valueTwo){
+          equal = 'son iguales';
+        } else{
+          equal = ' no son iguales';
+          break;
+        };
+    
+      };
+      
+      console.log(equal);
+    } else{
+        console.log( 'no son iguales')
+        
+    };
+  };
+  const object1= {
+      name: 'Kike',
+      age: '25'
+  }
+  const object2= {
+      name: 'Kike',
+      age: '25',
+      nacionality: 'colombia'
+  }
+  objectEqual(object1, object2);
