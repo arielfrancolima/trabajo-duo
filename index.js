9c3c5d7cb184f693635a09e60d17dcdfd2546768
arielfrancolima <ghazijj98@gmail.com>
2020-02-10 16:14:21 -0300

    hice la funcion pero esta a media

--- index.js
+++ index.js
@@ -11,20 +11,29 @@ function lista(lsita1){
     lista.map 
 }
 
-//Ejercicio 2 
+//Ejercicio 2
+/* 
 function definirFechas (){
     let fecha = new Date ();
     let dia = fecha.getDate();
     let mes = fecha.getMonth();
     let anio = fecha.getFullYear(); 
 
-    if (dia < 10) { dia= '0'+ dia );
+    if (dia < 10) { dia= '0'+ dia);
     if (mes < 10) {mes = '-'}
     }
+*/
+//Ejercicio 3 
+
+
+function daysToFinishYear (){ 
+date = new Date();
+cantDias = 31; 
+let day = fecha.getDate() + cantDias;//metodo que indica el dia actual
+const year = 366; 
+anio - cantDias;
+
+console.log (year - day);
+    
 }
 
-
-
-
-
-}
