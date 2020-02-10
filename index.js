7ab54b914a4f598aff4f36b088898d4b0569c52b
arielfrancolima <ghazijj98@gmail.com>
2020-02-10 14:35:54 -0300

    ya realize el ejercicio 1

---
+++ index.js
@@ -0,0 +1,12 @@
+function lista(lista){
+    let listaDuplicada = [];
+    for (i=0; i<lista.length; i++){
+        listaDuplicada.push (lista[i] * 2); 
+    }
+    return listaDuplicada; 
+    console.log(listaDuplicada);
+}
+
+function lista(lsita1){
+    lista.map 
+}
